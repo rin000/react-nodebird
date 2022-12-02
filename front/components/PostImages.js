5b7440961debd32128f2b5a3fc10edd232571ab4
rin000 <https://github.com/rin000/hello-github.git>
2022-12-03 02:39:14 +0900

    backUrl

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
 import ImagesZoom from './imagesZoom';
+import { backUrl } from '../config/config';
 
 const PostImages = ({ images }) => {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -16,7 +17,7 @@ const PostImages = ({ images }) => {
   if (images.length === 1) {
     return (
       <>
-        <img role="presentation" src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
@@ -24,8 +25,8 @@ const PostImages = ({ images }) => {
   if (images.length === 2) {
     return (
       <>
-        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`http://localhost:3065/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${backUrl}/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
@@ -33,7 +34,7 @@ const PostImages = ({ images }) => {
   return (
     <>
       <div>
-        <img role="presentation" style={{ width: '50%' }} src={`http://localhost:3065/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%' }} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
         <div
           role="presentation"
           style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
--- front/pages/about.js
+++ front/pages/about.js
@@ -1,62 +1,62 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Head from 'next/head';
-import { END } from 'redux-saga';
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import Head from 'next/head';
+// import { END } from 'redux-saga';
 
-import { Avatar, Card } from 'antd';
-import AppLayout from '../components/AppLayout';
-import wrapper from '../store/configureStore';
-import { LOAD_USER_REQUEST } from '../reducers/user';
+// import { Avatar, Card } from 'antd';
+// import AppLayout from '../components/AppLayout';
+// import wrapper from '../store/configureStore';
+// import { LOAD_USER_REQUEST } from '../reducers/user';
 
-const About = () => {
-const { userInfo } = useSelector((state) => state.user);
+// const About = () => {
+// const { userInfo } = useSelector((state) => state.user);
 
-return (
-    <AppLayout>
-    <Head>
-        <title>ZeroCho | NodeBird</title>
-    </Head>
-    {userInfo
-        ? (
-        <Card
-            actions={[
-            <div key="twit">
-                게시글
-                <br />
-                {userInfo.Posts}
-            </div>,
-            <div key="following">
-                팔로잉
-                <br />
-                {userInfo.Followings}
-            </div>,
-            <div key="follower">
-                팔로워
-                <br />
-                {userInfo.Followers}
-            </div>,
-            ]}
-        >
-            <Card.Meta
-            avatar={<Avatar>{userInfo.nickname[0]}</Avatar>}
-            title={userInfo.nickname}
-            description="노드버드 매니아"
-            />
-        </Card>
-        )
-        : null}
-    </AppLayout>
-);
-};
+// return (
+//     <AppLayout>
+//     <Head>
+//         <title>ZeroCho | NodeBird</title>
+//     </Head>
+//     {userInfo
+//         ? (
+//         <Card
+//             actions={[
+//             <div key="twit">
+//                 게시글
+//                 <br />
+//                 {userInfo.Posts}
+//             </div>,
+//             <div key="following">
+//                 팔로잉
+//                 <br />
+//                 {userInfo.Followings}
+//             </div>,
+//             <div key="follower">
+//                 팔로워
+//                 <br />
+//                 {userInfo.Followers}
+//             </div>,
+//             ]}
+//         >
+//             <Card.Meta
+//             avatar={<Avatar>{userInfo.nickname[0]}</Avatar>}
+//             title={userInfo.nickname}
+//             description="노드버드 매니아"
+//             />
+//         </Card>
+//         )
+//         : null}
+//     </AppLayout>
+// );
+// };
 
-export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-console.log('getServerSideProps');
-context.store.dispatch({
-    type: LOAD_USER_REQUEST,
-    data: 1,
-});
-context.store.dispatch(END);
-await context.store.sagaTask.toPromise();
-});
+// export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+// console.log('getServerSideProps');
+// context.store.dispatch({
+//     type: LOAD_USER_REQUEST,
+//     data: 1,
+// });
+// context.store.dispatch(END);
+// await context.store.sagaTask.toPromise();
+// });
 
-export default About;
+// export default About;

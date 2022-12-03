f347507f58d64a4a1328caa52808ecf5dec7a9e3
rin000 <https://github.com/rin000/hello-github.git>
2022-12-03 14:11:01 +0900

    80

--- back/app.js
+++ back/app.js
@@ -68,6 +68,6 @@ app.use('/post', postRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(85, () => {
+app.listen(80, () => {
     console.log('서버 실행중!');
 });

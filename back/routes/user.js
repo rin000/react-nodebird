a1bec7fe5bc4be195f3ba8c809ff031afa56eb5b
rin000 <engo1@naver.com>
2023-07-16 01:33:04 +0900

    23.7.16

--- back/routes/user.js
+++ back/routes/user.js
@@ -215,7 +215,7 @@ router.post('/', isNotLoggedIn, async (req, res, next) => {
         if (exUser) {
             return res.status(403).send('이미 사용중인 아이디 입니다.');
         }
-        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         await User.create({
             email: req.body.email,
             nickname: req.body.nickname,

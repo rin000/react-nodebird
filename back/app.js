4b7beba703c4dafcef1c52a5369173ac4a0a770b
rin000 <engo1@naver.com>
2023-07-23 13:49:21 +0900

    23.7.23

--- back/app.js
+++ back/app.js
@@ -68,6 +68,6 @@ app.use('/post', postRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(3070, () => {
+app.listen(3065, () => {
     console.log('서버 실행중!');
 });
--- front/config/config.js
+++ front/config/config.js
@@ -1,2 +1,1 @@
-// export const backUrl = 'http://3.34.46.106';
-export const backUrl = 'localhost:3065';
+exports.backUrl = 'http://localhost:3065';
